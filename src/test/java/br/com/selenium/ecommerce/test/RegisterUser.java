c29914cd09d9bcfe3f7ce3e5fa6bd152f02288a3
Denis Coelho <deniscoelho.dev@gmail.com>
2022-08-04 14:04:18 -0300

    atualizacao

--- src/test/java/br/com/selenium/ecommerce/test/RegisterUser.java
+++ src/test/java/br/com/selenium/ecommerce/test/RegisterUser.java
@@ -1,5 +1,7 @@
 package br.com.selenium.ecommerce.test;
 
+import java.time.Duration;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,20 +23,28 @@ public class RegisterUser {
 		String title = driver.getCurrentUrl();
 		System.out.println(title);
 
+		// *******************************************************************************************
+		// 4
 		WebElement signBtn = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a/i"));
 		signBtn.click();
 
+		// *******************************************************************************************
+		// 5
 		Boolean newUserSignupIsVisible = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/h2"))
 				.isDisplayed();
 		System.out.println("new User Signup Is Visible: " + newUserSignupIsVisible);
 
+		// *******************************************************************************************
+		// 6 e 7
 		WebElement nameField = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]"));
 		WebElement emailField = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]"));
 		WebElement signup = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button"));
 		nameField.sendKeys("Maria");
-		emailField.sendKeys("maria@email.com");
+		emailField.sendKeys("maria3@email.com");
 		signup.click();
 
+		// *******************************************************************************************
+		// 8
 		Boolean enterAccountInformarionsIsVisible = driver
 				.findElement(By.xpath("//*[@id=\"form\"]/div[1]/div/div/div/h2/b")).isDisplayed();
 		System.out.println("enter account information Is Visible: " + enterAccountInformarionsIsVisible);
@@ -56,7 +66,56 @@ public class RegisterUser {
 		WebElement specialOffers = driver.findElement(By.xpath("//*[@id=\"optin\"]"));
 		specialOffers.click();
 
-//		parei no item 12
+		// *******************************************************************************************
+		// 12
+		WebElement firstNameAdress = driver.findElement(By.xpath("//*[@id=\"first_name\"]"));
+		WebElement lastNameAdress = driver.findElement(By.xpath("//*[@id=\"last_name\"]"));
+		WebElement company = driver.findElement(By.xpath("//*[@id=\"company\"]"));
+		WebElement adress1 = driver.findElement(By.xpath("//*[@id=\"address1\"]"));
+		WebElement adress2 = driver.findElement(By.xpath("//*[@id=\"address2\"]"));
+		Select country = new Select(driver.findElement(By.name("country")));
+		WebElement state = driver.findElement(By.xpath("//*[@id=\"state\"]"));
+		WebElement city = driver.findElement(By.xpath("//*[@id=\"city\"]"));
+		WebElement zipcode = driver.findElement(By.xpath("//*[@id=\"zipcode\"]"));
+		WebElement mobileNumber = driver.findElement(By.xpath("//*[@id=\"mobile_number\"]"));
+		firstNameAdress.sendKeys("Maria");
+		lastNameAdress.sendKeys("Joaquina");
+		company.sendKeys("Capgemini");
+		adress1.sendKeys("st 1211,");
+		adress2.sendKeys("complemento altos");
+		country.selectByVisibleText("Canada");
+		state.sendKeys("Alberta");
+		city.sendKeys("Vitoria");
+		zipcode.sendKeys("9281829827");
+		mobileNumber.sendKeys("8383939838");
+
+		// *******************************************************************************************
+		// 13
+		WebElement createAccount = driver.findElement(By.xpath("//*[@id=\"form\"]/div[1]/div/div/div/form/button"));
+		createAccount.click();
+
+		// *******************************************************************************************
+		// 14
+		Boolean accountCreatedIsVisible = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/h2/b"))
+				.isDisplayed();
+		System.out.println("Account created is visible: " + accountCreatedIsVisible);
+
+		// *******************************************************************************************
+		// 15
+		WebElement continueBtn = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/div/a"));
+		continueBtn.click();
+
+		// *******************************************************************************************
+		// 16
+		Boolean userNameLogged = driver
+				.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[10]/a/b")).isDisplayed();
+		System.out.println("User name visible: " + userNameLogged);
+
+		// *******************************************************************************************
+		// 17
+		WebElement deleteAccount = driver
+				.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[5]/a"));
+		deleteAccount.click();
 
 //		driver.quit();
 //		System.out.println("Script executado com sucesso!");
